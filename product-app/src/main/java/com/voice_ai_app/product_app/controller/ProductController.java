bd5942b5e28ebee54a9f4385c05c6731ace0b63b
manivannancsmca <manivannancsmca@gmail.com>
2026-08-25 22:26:01 +0530

    added argument constructor

--- product-app/src/main/java/com/voice_ai_app/product_app/controller/ProductController.java
+++ product-app/src/main/java/com/voice_ai_app/product_app/controller/ProductController.java
@@ -8,17 +8,16 @@ import com.voice_ai_app.product_app.model.Product;
 import com.voice_ai_app.product_app.service.ProductService;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
-
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
 
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
